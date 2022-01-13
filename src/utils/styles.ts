e465e45199887ba2e9d1022e90221a6025f5fa06
Mercy Star <mercystar20@gmail.com>
2022-01-13 08:50:44 +0100

    Initial Commit

---
+++ src/utils/styles.ts
@@ -0,0 +1,13 @@
+export const DEFAULT_COLOR = "#E0E0E0";
+export const DEFAULT_DARK_COLOR = "#AEAEAE";
+export const PRIMARY_COLOR = "#2962FF";
+export const PRIMARY_DARK_COLOR = "#0039CB";
+export const SECONDARY_COLOR = "#455A64";
+export const SECONDARY_DARK_COLOR = "#1C313A";
+export const DANGER_COLOR = "#D32F2F";
+export const DANGER_DARK_COLOR = "#9A0007";
+export const DARK_TEXT = "#3F3F3F";
+export const PRIMARY_TEXT = "#3D5AFE";
+export const WHITE_COLOR = "#FFFFFF"
+
+
